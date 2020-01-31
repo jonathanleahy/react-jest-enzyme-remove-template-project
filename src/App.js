dab5e158964f811ede63f55253cfe12f167ed9ae
Jonathan Leahy <jonathanl@newtimesystems.com>
2020-01-31 22:26:39 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -2,26 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const title = "Learn112 React"
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {title}
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const title='ss'
+    return (
+        <div data-test='component-app'>
+          <h1>Hello</h1>
+            sss
+        </div>
+    );
+  }
 }
 
 export default App;
